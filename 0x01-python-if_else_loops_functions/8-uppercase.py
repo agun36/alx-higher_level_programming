efcf7b3dfc1dedb82179c9dfd80831b6e6aeb52d
agun36 <aguntoroti10@gmail.com>
2023-05-09 15:39:49 +0000

    is_lower script

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        print(chr(ord(c) - 32), end="")
-     print()    
+        ascii_val = ord(c)
+        if ascii_val >= 97 and ascii_val <= 122:
+            ascii_val -= 32
+        print("{}".format(chr(ascii_val)), end="")
+    print("")
 
