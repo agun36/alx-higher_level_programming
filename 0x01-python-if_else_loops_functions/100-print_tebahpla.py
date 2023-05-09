42d631b63f681bfbdd934d251057d21f53dd2c59
agun36 <aguntoroti10@gmail.com>
2023-05-09 18:01:40 +0000

    is_lower script

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 64, -1):
-    char = chr(i)
+for i in range(90, 64, -1):
     if i % 2 == 0:
-        char = char.upper()
-    print(char, end="")
+        letter = chr(i+32)
+    else:
+        letter = chr(i)
+    print("{}".format(letter), end="")
