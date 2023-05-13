8941800fea1b0499d78f47d5642c4570d4d44755
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-05-13 10:03:01 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     
     if idx < 0 or idx > len(my_list):
-        return my_list
+        return my_list.copy
     else:
         new_list = my_list.copy()
         new_list[idx] = element
