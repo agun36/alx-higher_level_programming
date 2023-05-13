c2d0dc9e87d1e3a9eddc734fd81614582ffee284
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-05-13 13:34:09 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,8 @@
 def max_integer(my_list):
     if len(my_list) == 0:
         return None
-
     big_items = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > big_items:
             big_items = my_list[i]
-    
     return big_items
