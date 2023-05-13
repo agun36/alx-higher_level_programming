77ccc0c3d5b7d117855edaed739c5a85ea561094
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-05-13 12:24:11 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,34 +1,72 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
+
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int length = 0;
-	int count = 0;
-	int result = 1;
+	listint_t *t, *r, *m;
+	size_t s = 0, i;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	while (current != NULL)
+	t = *head;
+	while (t)
 	{
-		length++;
-		current = current->next;
+		s++;
+		t = t->next;
 	}
 
-	current = *head;
+	t = *head;
+	for (i = 0; i < (s / 2) - 1; i++)
+		t = t->next;
 
-	while (count < length / 2)
+	if ((s % 2) == 0 && t->n != t->next->n)
+		return (0);
+
+	t = t->next->next;
+	r = reverse_listint(&t);
+	mid = r;
+
+	t = *head;
+	while (r)
 	{
-		if (current->n != current->next->n)
-		{
-			result = 0;
-           		break;
-		}
-        current = current->next->next;
-        count++;
+		if (t->n != r->n)
+			return (0);
+		t = t->next;
+		r = r->next;
 	}
+	reverse_listint(&m);
 
-	return (result);
+	return (1);
 }
