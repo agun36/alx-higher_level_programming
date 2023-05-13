c7abf0f5922f041d5d28a362f48a68eb5e130fca
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-05-13 10:19:33 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-  new_string = ""
-  for char in my_string:
-    if char != 'c' and char != 'C':
-      new_string += char
-  return new_string
+    new_string = ""
+    for char in my_string:
+        if ord(char) != ord("c") and ord(char) != ord("C"):
+            new_string += char
+            return new_string
