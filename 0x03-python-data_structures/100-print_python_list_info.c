b385518223ee00ff0b36ad017f9a5d209baa1db7
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-05-13 12:31:45 +0100

    Create 100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,28 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - Prints basic info about Python lists
+ * @p: A PyObject items.
+ *
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+    int list_s, all, i;
+    PyObject *list_o;
+
+    list_s = Py_SIZE(p);
+    all = ((PyListObject *)p)->all;
+
+    printf("[*] Size of the Python List = %d\n", list_s);
+    printf("[*] Allocated = %d\n", all);
+
+    for (i = 0; i < list_s; i++)
+    {
+        printf("Element %d: ", i);
+
+        list_o = PyList_GetItem(p, i);
+        printf("%s\n", Py_TYPE(list_o)->tp_name);
+    }
+}
