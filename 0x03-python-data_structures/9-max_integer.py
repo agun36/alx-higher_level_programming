bb1542d47293f9ddce20cd60394403a5ef85bdda
agun36 <aguntoroti10@gmail.com>
2023-05-13 10:33:48 +0000

    6-print_matrix_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-  if not my_list:
-    return None
-  else:
-    max_int = my_list[0]
+    if not my_list:
+        return None
+    max_int = 0
     for item in my_list:
-      if item > max_int:
-        max_int = item
+        if item > max_int:
+            max_int = item
     return max_int
