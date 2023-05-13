89cdeece13ada02092226c7409cdc2208318cbf3
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-05-13 12:36:58 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if  idx < 0 or idx >= len(my_list):
-        del my_list[idx]
-    return my_list
+     if len(my_list) == 0:
+        return (None)
+
+    big_items = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big_items:
+            big_items = my_list[i]
+
+    return (big_items)
