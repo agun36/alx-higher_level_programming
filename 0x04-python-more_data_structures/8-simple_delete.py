4893002cd8f828b0b9af4b14d94879f30330ec1c
agun36 <aguntoroti10@gmail.com>
2023-05-17 13:06:54 +0000

    7-update_dictionary.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,9 +3,3 @@ def simple_delete(a_dictionary, key=''):
     if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
-
-
-def print_sorted_dictionary(a_dictionary, key=''):
-    for key in sorted(a_dictionary.keys()):
-        print(key + ':', a_dictionary[key])
-
