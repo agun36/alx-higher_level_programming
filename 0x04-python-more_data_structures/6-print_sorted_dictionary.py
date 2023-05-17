7d12415e278c8bdf2f08c053c5b379160cd8c22e
agun36 <aguntoroti10@gmail.com>
2023-05-17 12:57:49 +0000

    7-update_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
     for key, value in sorted(a_dictionary.items()):
         print(key + ':', value)
 
