6e4197bde69edae10db80d51b55c66919b886d2c
agun36 <aguntoroti10@gmail.com>
2023-05-29 16:04:29 +0000

    python files just added

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,6 +5,7 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
+            
             result += a ** b / i
         except:
             result = b + a
