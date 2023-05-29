f9aabc3372204f19c4d52709274b9280c3850302
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-05-29 19:30:43 +0100

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/urs/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count_num = 0
+    counts = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
@@ -8,4 +8,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except(ValueError, TypeError):
             pass
     print()
-    return (count_num)
+    return (counts)
