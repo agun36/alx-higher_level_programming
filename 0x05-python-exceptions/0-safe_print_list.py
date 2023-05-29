a4c6816541b968ef885f79289d4ea5bf7917e849
agun36 <aguntoroti10@gmail.com>
2023-05-29 12:47:02 +0000

    just correct to meet the failing requirements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
                 break
             print("{}".format(i), end="")
             count += 1
-    except Exception as e:
-        print ("An error occured:",str(e))
+    except IndexError:
+        pass
     print()
     return (count)
