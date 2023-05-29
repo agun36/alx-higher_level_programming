c74c07eacf9169385468803bb09289e725b91513
agun36 <aguntoroti10@gmail.com>
2023-05-29 11:21:16 -0700

    4-list_division.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,8 @@ def safe_print_list_integers(my_list=[], x=0):
     count_num = 0
     for i in range(x):
         try:
-             print("{:d}".format(my_list[i]), end="")
-             count_num += 1
+            print("{:d}".format(my_list[i]), end="")
+            count_num += 1
         except(TypeError, ValueError):
             continue
     print()
