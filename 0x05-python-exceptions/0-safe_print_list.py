1feebcce8b74b3381aa5b60ad468567365a26b5a
agun36 <aguntoroti10@gmail.com>
2023-05-29 12:33:59 +0000

    just add the directiory and some files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,6 @@ def safe_print_list(my_list=[], x=0):
             output += str(i)
             count += 1
     except Exception as e:
-        print ("Error", str(e))
+        print ("Error {:d}".format(str(e)))
     print(output)
     return (count)
