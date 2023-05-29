01a26ce2fddb41f1d45d3bbd7e93723e2a99e326
agun36 <aguntoroti10@gmail.com>
2023-05-29 14:11:20 +0000

    2-safe_print_list_inegers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
              print("{:d}".format(my_list[i]), end="")
              count += 1
         except (TypeError, ValueError):
-            print("An error occurred: Invalid type for my_list or x")
+            break
     print()
     return (count)
