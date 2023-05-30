bd6ec8472886c26e4262343c16d6e0fea8fe730a
agun36 <aguntoroti10@gmail.com>
2023-05-30 06:22:36 -0700

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""define a class of square """
+
+
+class Square():
+    """initializing private using __size
+    Args:
+	__size (int): size of the quare
+    Return: None
+    """
+    def __init__(self, size):
+        self.__size = size
