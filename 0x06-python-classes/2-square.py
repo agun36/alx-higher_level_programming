5bfa6c537f573e9f2a827d2911fa99b3695150a2
agun36 <aguntoroti10@gmail.com>
2023-05-30 07:36:05 -0700

    sqare

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""define a class of square """
+
+
+class Square():
+    """initializing private using __size
+    Attributes:
+       __size (int): size of the quare
+    """
+    def __init__(self, size=0):
+        """assigning values to private attribute
+        Args:
+             size (int): size of a quare
+        Returns: None
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
