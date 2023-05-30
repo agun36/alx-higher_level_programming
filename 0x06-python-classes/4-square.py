b7f944a34fef77563e9a3195dabd251aa06d741b
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-05-30 17:01:40 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,7 +2,6 @@ class Square:
     def __init__(self, size=0):
         """
         Initializes a Square instance.
-
         Args:
             size (int): The size of the square (default is 0).
         """
@@ -10,7 +9,6 @@ class Square:
      def area(self):
         """
         Calculates the area of the square.
-
         Returns:
             int: The area of the square.
         """
