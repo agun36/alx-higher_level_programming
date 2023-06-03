e2c3ab48c8ba80e6209920fd594ee71937642217
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-03 16:39:23 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,50 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Module containing matrix_divided function.
+
+"""
+
 def matrix_divided(matrix, div):
     """
-    Divides elements of a matrix by a number.
+    Divides all elements of a matrix by a divisor.
 
     Args:
-        matrix (list): List of lists of ints or floats.
-        div (int/float): Number to divide matrix elements.
+        matrix (list): List of lists containing integers or floats.
+        div (int or float): Divisor.
 
     Returns:
-        list: New matrix with elements divided by number.
+        list: New matrix with elements divided by the divisor.
 
     Raises:
-        TypeError: If matrix is not a matrix of ints/floats.
-        ZeroDivisionError: If div is zero.
+        TypeError: If matrix is not a list of lists of integers or floats,
+            or if div is not a number.
+        ZeroDivisionError: If div is equal to 0.
 
     """
 
-    if not all(isinstance(row, list) and all(isinstance(num, (int, float)) for num in row) for row in matrix):
+    # Check if matrix is a list of lists
+    if not isinstance(matrix, list) or any(not isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+
+    # Check if all rows have the same size
+    row_size = len(matrix[0])
+    if any(len(row) != row_size for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
+
+    # Check if div is a number
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
+    # Check if div is not equal to 0
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
+    # Perform division and round to 2 decimal places
     new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+
     return new_matrix
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
