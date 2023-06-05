f3eec5ccba753a06356355413638f9a12117ac5d
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-05 17:03:37 +0100

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -13,6 +13,7 @@ class Rectangle:
             width (int): The with of the new rectangle.
             height (int): The height of the new rectangle.
         """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
