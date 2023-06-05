62c9b8fed319c4fa161b8568f94f421bdbbd78d6
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-05 18:12:05 +0100

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,95 +1,77 @@
 #!/usr/bin/python3
-"""Definning a Rectangle class"""
+import sys
 
 
-class Rectangle:
-    """represents a rectanglle."""
+def is_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]"""
+    # Check the row on the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
 
-    number_of_instances = 0
-    print_symbol = "#"
+    # Check the upper diagonal on the left side
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
 
-    def __init__(self, width=0, height=0):
-        """creating instantiation
-        Args:
-            width (int): The with of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
-        type(self).number_of_instances += 1
-        self.width = width
-        self.height = height
+    # Check the lower diagonal on the left side
+    i, j = row, col
+    while i < N and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i += 1
+        j -= 1
 
-    @property
-    def width(self):
-        """Get or set the width"""
-        return self.__width
+    return True
 
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
-    @property
-    def height(self):
-        """Get or set the height of the rectangle."""
-        return self.__height
+def solve_nqueens(board, col):
+    """Recursive function to solve the N Queens problem"""
+    # If all queens are placed, print the solution
+    if col >= N:
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        print(solution)
+        return True
 
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+    # Consider this column and try placing the queen in all rows
+    for row in range(N):
+        if is_safe(board, row, col):
+            # Place the queen in the current position
+            board[row][col] = 1
 
-    def area(self):
-        """Return the area of the Rectangle."""
-        return (self.__width * self.__height)
+            # Recur to place the rest of the queens
+            solve_nqueens(board, col + 1)
 
-    def perimeter(self):
-        """Return the perimeter of the Rectangle."""
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
+            # Backtrack and remove the queen from the current position
+            board[row][col] = 0
 
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
 
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return (rect_1)
-        return (rect_2)
+# Check the number of command-line arguments
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-    @classmethod
-    def square(cls, size=0):
-        return (cls(size, size))
+# Get the value of N from the command-line argument
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-    def __str__(self):
-        """Return the printable representation of the Rectangle.
-        Represents the rectangle with the # character.
-        """
-        if self.__width == 0 or self.__height == 0:
-            return ("")
+# Check if N is at least 4
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-        re = []
-        for i in range(self.__height):
-            [re.append(str(self.print_symbol)) for j in range(self.__width)]
-            if i != self.__height - 1:
-                re.append("\n")
-        return ("".join(re))
+# Create an empty chessboard
+chessboard = [[0 for _ in range(N)] for _ in range(N)]
 
-    def __repr__(self):
-        """Return the string representing the Rectangle."""
-        re = "Rectangle(" + str(self.__width)
-        re += ", " + str(self.__height) + ")"
-        return (re)
-
-    def __del__(self):
-        """Print a message for every deleted  Rectangle."""
-        type(self).number_of_instances -= 1
-        print("Bye rectangle...")
+# Solve the N Queens problem
+solve_nqueens(chessboard, 0)
