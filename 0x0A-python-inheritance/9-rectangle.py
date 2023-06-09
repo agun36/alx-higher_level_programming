27fb547f7d7a760f1461acf1a09170e45044df57
agun36 <aguntoroti10@gmail.com>
2023-06-09 14:30:36 -0700

    i just add some base and subclass instance

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+class Rectangle(BaseGeometry):
+    """defining a rectangle using Base."""
+
+    def __init__(self, width, height):
+        """creating  attributes with super.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
