a0a4d104d10d865ea25d30d002202bb64190ad2f
agun36 <aguntoroti10@gmail.com>
2023-06-13 10:24:00 -0700

    class Student that defines a student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,12 +6,7 @@ class Student:
     """property of a student"""
 
     def __init__(self, first_name, last_name, age):
-         """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+         """Initialize a  new student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
