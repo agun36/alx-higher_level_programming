03a7a0f55980bb1ed4d27ad6d868b50c7ae1dfef
agun36 <aguntoroti10@gmail.com>
2023-06-13 10:19:49 -0700

    class Student that defines a student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,7 +11,12 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance"""
+        """Get a dictionary Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
         if (type(attrs) == list and 
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
