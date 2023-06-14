9a66fb5b5ead381c0aa1d549133378722bbdc2c7
agun36 <aguntoroti10@gmail.com>
2023-06-14 00:26:03 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,15 +6,15 @@ class Student:
     """property of a student"""
 
     def __init__(self, first_name, last_name, age):
-         """Initialize a  new student
-          Args:
+        """Initialize a  new student
+        Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
             age (int): The age of the student.
-         """
-         self.first_name = first_name
-         self.last_name = last_name
-         self.age = age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         """Get a dictionary Student.
