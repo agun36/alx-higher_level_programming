56487962524417cc8f76deee68b3a44a8ed9bf1b
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-15 09:50:11 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,7 @@ class Rectangle(Base):
         """Display the rectangle using '#' character."""
         for _ in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """Return the string representation of the rectangle."""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
