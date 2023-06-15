a183e4e78f0b1c3c575dc248e73cd360ada4b4ee
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-15 11:16:57 +0100

    Update square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize a new instance of the Square class.
+        Args:
+            size (int): The size of the square (width and height).
+            x (int): The x-coordinate of the square (default: 0).
+            y (int): The y-coordinate of the square (default: 0).
+            id (int): The id of the square (optional).
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter method for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter method for size."""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return the string representation of the square."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """Assign arguments to the attributes."""
+        if args:
+            attributes = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attributes[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
