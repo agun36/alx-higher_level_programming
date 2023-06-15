35e2720c4f12387f73929cf2bf9ddd99c11f466c
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-15 11:27:55 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -41,3 +41,14 @@ class Square(Rectangle):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def update(self, *args, **kwargs):
+    """Update the attributes of the square."""
+    if args:
+        attributes = ["id", "size", "x", "y"]
+        for i, arg in enumerate(args):
+            if i < len(attributes):
+                setattr(self, attributes[i], arg)
+    else:
+        for key, value in kwargs.items():
+            setattr(self, key, value)
