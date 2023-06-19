60c6e3af283ca058d70de65f18a9e577d2876a1b
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-19 11:05:43 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,51 +1,61 @@
 #!/usr/bin/python3
+"""This is the Square module.
+Contains the Square class that inherits from Rectangle.
 """
-Square class inherits from Rectangle class with some attributes
-"""
-
 from models.rectangle import Rectangle
-"""importing the Rectangle module"""
 
 
 class Square(Rectangle):
-    """class Square(subclass)from Rectangle class"""
-    def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
-        self.size = size
+    """This class inherits from Rectangle and defines a Square object."""
 
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes the default attributes of the Base object.
+        Args:
+            size (int): the size of a square side.
+            x (int): the wanted horizontal (x) padding of the square.
+            y (int): the wanted vertical (y) padding of the square.
+            id (int): the wanted identifier of the Base object.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Overrides the default behaviour of the __str__ method."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
+
+    # size attribute getter and setter.
     @property
     def size(self):
-        """gets the size of the square"""
+        """Get and Set the size attribute of the Square."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """sets the size(width&height) of square"""
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """Defines a format for the string representation of the class"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
-
-    def __update(self, id=None, size=None, x=None, y=None):
-        """Internal method that updates instance attributes with */**args"""
-        if id is not None:
-            self.id = id
-        if size is not None:
-            self.size = size
-        if x is not None:
-            self.x = x
-        if y is not None:
-            self.y = y
-
+    # Methods
     def update(self, *args, **kwargs):
-        """Updating instance attributes using non-keyword & keyword args"""
-        if args:
-            self.__update(*args)
-        elif kwargs:
-            self.__update(**kwargs)
+        """Updates the Square attributes.
+        Args:
+            args (list): attributes to be modified [id, size, x, y].
+            kwargs (dict): attributes to be modified.
+        """
+        dct = {}
+        if args is not None and len(args) > 0:
+            keys = ['id', 'size', 'x', 'y']
+            for i in range(len(args) if len(args) <= 4 else 4):
+                dct[keys[i]] = args[i]
+        else:
+            dct = kwargs
+
+        if len(dct) > 0:
+            for key, value in dct.items():
+                if key == 'id' and value is None:
+                    self.__init__(self.size, self.x, self.y)
+                else:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
-        """Returns dictionary representation of a Rectangle attributes"""
-        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
+        """Returns the dictionary representation of a Square"""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
