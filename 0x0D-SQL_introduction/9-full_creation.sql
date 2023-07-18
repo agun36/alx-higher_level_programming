8068c6cd1130bc338fab8416e925430a9229e645
agun36 <aguntoroti10@gmail.com>
2023-07-18 11:36:27 -0700

    main

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- creates a table second_table in the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (
-`id` INT,
-`name` VARCHAR(256),
-`score` INT,
+    id INT,
+    name VARCHAR(256),
+    score INT,
 );
 INSERT INTO `second_table` VALUES
     (1, 'john', 10),
