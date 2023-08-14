30a52a8c2a49609c61f15452596a6485a49b59c7
agun36 <agunakindele10@gmail.com>
2023-08-14 17:15:59 +0000

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let num = 0;
+const x = Number(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  while (num < x) {
+    console.log('C is fun');
+    num++;
+  }
+}
