eb1836c1c2ecd4823b074050996a95b63c335ab7
agun36 <agunakindele10@gmail.com>
2023-08-14 15:07:29 +0000

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if(args.length === 0){
+if (args.length === 0){
  console.log("No argument");
 } else if (args.length === 1) {
   console.log("Argument found");
