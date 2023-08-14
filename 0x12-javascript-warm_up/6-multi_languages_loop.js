59ea9c2f1e2f33d1cd49db454e69fb82f903071b
agun36 <agunakindele10@gmail.com>
2023-08-14 16:59:43 +0000

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arrList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-num = 0;
+let num = 0;
 while (num < arrList.length) {
   console.log(arrList[num]);
   num++;
