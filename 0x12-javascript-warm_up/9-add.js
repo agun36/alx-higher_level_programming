58a6e97342bd690c06f4f09be9cd94b7a188e790
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-08-14 19:56:15 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const arg1 = Number(process.argv[2]);
-const arg2 = Number(process.argv[3]);
-function add(a, b) {
-  const output = arg1 + arg2;
-  console.log(output);
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  }
+  return a + b;
 }
-add(arg1, arg2);
+console.log(add(first, second));
