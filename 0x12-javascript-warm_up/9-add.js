0309c1672802ccdc373268cecfde95225b19484c
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-08-14 19:54:49 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const arg1 = Number(process.argv[2]);
 const arg2 = Number(process.argv[3]);
-
 function add(a, b) {
   const output = arg1 + arg2;
   console.log(output);
 }
-
 add(arg1, arg2);
