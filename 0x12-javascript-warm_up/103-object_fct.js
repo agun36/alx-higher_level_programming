8366b51a036144f7e5ca64aad2408fc7b20ad472
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-08-14 21:13:58 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,11 @@
+#!/usr/bin/node
 const myObject = {
   type: 'object',
   value: 12,
-  incr() {
-    myObject.value++;
+  incr: function () {
+    this.value++;
   }
 };
-
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
