f8413ea977a3566345a962c6f2d2094eef94ab3c
agun36 <agunakindele10@gmail.com>
2023-08-14 17:17:57 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ let num = 0;
 const x = Number(process.argv[2]);
 
 if (isNaN(x)) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   while (num < x) {
     console.log('C is fun');
