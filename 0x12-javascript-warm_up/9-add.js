8d768b4aa09b84ce8d97af23b34e4b13ac7de53f
agun36 <agunakindele10@gmail.com>
2023-08-14 18:39:35 +0000

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const arg1 = Number(process.argv[2]);
 const arg2 = Number(process.argv[3]);
+
 function add(a, b) {
   const output = arg1 + arg2;
   console.log(output);
 }
+
 add(arg1, arg2);
