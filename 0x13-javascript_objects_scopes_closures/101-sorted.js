cd9417212476a14e7016004d358fddc1ac5cea45
agun36 <agunakindele10@gmail.com>
2023-08-15 17:07:19 +0000

    main

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const dict_new = {};
+const keys = Object.keys(dict);
+let i = 0;
+while (i < keys.length) {
+  const mail= keys[i];
+  if (dict[mail] in dict_new) {
+    dict_new[dict[mail]].push(mail);
+  } else {
+    dict_new[dict[mail]] = [mail];
+  }
+  i++;
+};
+
+console.log(dict_new);
