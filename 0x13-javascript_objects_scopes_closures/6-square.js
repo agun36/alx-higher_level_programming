07f85b20319a274edd3e1dee897ce49b654729fd
agun36 <agunakindele10@gmail.com>
2023-08-15 15:28:59 +0000

    main

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,15 +2,15 @@
 const SquareBase = require('./5-square');
 
 module.exports = class Square extends SquareBase {
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
-    };
+    }
     // methhod that print rectangle with while loop
     let num = 0;
     while (num < this.height) {
       console.log(c.repeat(this.width));
       num++;
-    };
-  };
+    }
+  }
 };
