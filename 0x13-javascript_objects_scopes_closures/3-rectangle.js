13ed63d6127ab00f30b4146fc11b659d4869cab4
agun36 <agunakindele10@gmail.com>
2023-08-15 15:09:14 +0000

    main

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,12 +2,12 @@
 // create new instance with constructor
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || isNaN(w) || h <= 0 || isNaN(h)) {
+    if ((w <= 0 || isNaN(w)) || (h <= 0 || isNaN(h))) {
       return {};
-    }
+    };
     this.width = w;
     this.height = h;
-  }
+  };
 
   // methhod that print rectangle with while loop
   print () {
@@ -15,6 +15,6 @@ module.exports = class Rectangle {
     while (num < this.height) {
       console.log('X'.repeat(this.width));
       num++;
-    }
-  }
+    };
+  };
 };
