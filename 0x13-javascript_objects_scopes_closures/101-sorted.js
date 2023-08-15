1a45c2b2ed41e50e1b6150f35ba8d25cbea68d25
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-08-15 18:26:19 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
+const list = require('./101-data').dict;
+const sorted = {};
 
-const dict_new = {};
-const keys = Object.keys(dict);
-let i = 0;
-while (i < keys.length) {
-  const mail = keys[i];
-  if (dict[mail] in dict_new) {
-    dict_new[dict[mail]].push(mail);
-  } else {
-    dict_new[dict[mail]] = [mail];
-  }
-  i++;
+Object.keys(list).forEach(key => {
+  if (sorted[list[key]] === undefined) sorted[list[key]] = [];
+  sorted[list[key]].push(key);
 }
-
-console.log(dict_new);
+);
+console.log(sorted);
