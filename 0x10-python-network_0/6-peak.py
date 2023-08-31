ffccacbf17abace677bcaaeda962dbc708ea720c
agun36 <aguntoroti10@gmail.com>
2023-08-31 09:42:07 -0700

    main

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,13 +4,14 @@
 
 def find_peak(list_of_integers):
     """fid the list of number and look from the last """
+    mid = len(list_of_integers)
+
     if not list_of_integers:
         return None
     if len(list_of_integers) == 1:
         return list_of_integers[0]
     if len(list_of_integers) == 2:
         return max(list_of_integers)
-    mid = len(list_of_integers)
     if list_of_integers[mid] > list_of_integers[mid - 1] and
         list_of_integers[mid] > list_of_integers[mid + 1]:
         return list_of_integers[mid]
