16afc963cb001fedcb04f54203adadfe5c008ad1
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-09-01 16:55:47 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -21,7 +21,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 from sys import argv
 
-
 if __name__ == '__main__':
     url = 'https://api.github.com/users/{}'.format(argv[1])
     r = requests.get(url,
