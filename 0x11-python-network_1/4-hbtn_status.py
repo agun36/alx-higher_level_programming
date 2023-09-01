983cc6cb6d7c86fb611d15ef39c7833c981f6202
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-09-01 17:30:31 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -18,8 +18,8 @@ def hbtn_status():
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     print('Body response:')
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
 
 
 if __name__ == '__main__':
