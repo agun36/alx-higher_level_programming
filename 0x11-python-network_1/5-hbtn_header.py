5571637870c97fe8d78b82e2de97373ac015b9c9
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-09-01 17:38:53 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,12 +8,9 @@ The value of this variable is different for each request.
 
 Usage: ./5-hbtn_header.py <url>
 """
-
+import requests
+from sys import argv
 
 if __name__ == '__main__':
-    import requests
-    from sys import argv
-
     r = requests.get(argv[1])
     print(r.headers.get('X-Request-Id'))
-        print("Header 'X-Request-Id' not found in the response.")
