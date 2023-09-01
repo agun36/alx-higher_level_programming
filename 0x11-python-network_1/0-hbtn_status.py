ad7767da1c60408fe59663733cb3101d1f00e5ba
agun36 <aguntoroti10@gmail.com>
2023-09-01 05:08:18 -0700

    main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,24 +7,27 @@ and prints the type, content, and UTF-8 content of the response.
 
 import urllib.request
 
+
 def hbtn_status():
-  """
-  This function fetches the status of the HTB intranet and prints
-  the type, content, and UTF-8 content of the response.
+    """
+    This function fetches the status of the HTB intranet and prints
+    the type, content, and UTF-8 content of the response.
 
-  Args:
-    None
+    Args:
+        None
 
-  Returns:
-    None
-  """
+    Returns:
+        None
+    """
 
-  with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-      body = response.read()
-  print('Body response:')
-  print('\t- type: {}'.format(type(body)))
-  print('\t- content: {}'.format(body))
-  print('\t- utf8 content: {}'.format(body.decode('utf-8')))
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode('utf-8')))
+
 
 if __name__ == "__main__":
-  hbtn_status()
+    hbtn_status()
