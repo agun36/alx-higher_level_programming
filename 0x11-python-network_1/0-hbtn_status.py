d12f3734ff68461ad23675c5e98d970916df34e3
agun36 <aguntoroti10@gmail.com>
2023-09-01 04:50:27 -0700

    main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -23,7 +23,7 @@ def hbtn_status():
       body = response.read()
   print('Body response:')
   print('\t- type: {}'.format(type(body)))
-  print('\t- content: {}'.format(repr(body)))
+  print('\t- content: {}'.format(body))
   print('\t- utf8 content: {}'.format(body.decode('utf-8')))
 
 if __name__ == "__main__":
